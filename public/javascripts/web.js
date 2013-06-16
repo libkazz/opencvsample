efdb72d42b54f55f45003597eb433835f5dc4817
Kazuaki Takemoto <libkazz@gmail.com>
2013-06-16 13:11:06 +0900

    Integrate canvas area

--- public/javascripts/web.js
+++ public/javascripts/web.js
@@ -3,7 +3,6 @@ $(function() {
     console.log("capture photo");
     html2canvas($("#photo")[0], {
       onrendered: function(canvas){
-        $("#capture").append(canvas);
         share_wared_photo(canvas);
       },
       logging: false
@@ -13,13 +12,10 @@ $(function() {
   var share_wared_photo = function(canvas) {
     console.log("shared photo: " + canvas);
     $.post('/share', { img : canvas.toDataURL('image/jpg') }, function(data){
-      $("#capture").children().hide();
-      console.log(data);
       var src = JSON.parse(data)["src"];
       console.log("upload shared photo: " + src);
 
       $("#captures").append("<img src=\"" + src + "\" />");
-      $("#capture canvas").remove();
     });
   };
 
